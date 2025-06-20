d015ac15fe1f6c5da8a96b311f180c702319c766
Dominique Dessy <dominique.dessy@me.com>
2025-06-20 09:52:22 +0200

    updating gitignore

--- Arduino-MQTT-Unique-DeviceId.ino
+++ Arduino-MQTT-Unique-DeviceId.ino
@@ -15,7 +15,7 @@ const int RETRY_TIMEOUT = 2000;
 const long WIFI_CHECK_INTERVAL = 5000; // Vérification de la connexion WiFi toutes les 5 secondes
 
 byte deviceID = 0;
-char macAddress[18];  // XX:XX:XX:XX:XX:XX\0
+char macAddress[18]; // XX:XX:XX:XX:XX:XX\0
 char mqttBrokerIP[16] = "";
 WiFiUDP udp;
 unsigned long lastRetryTime = 0;
@@ -77,74 +77,83 @@ void setup()
     // Obtenir l'adresse MAC formatée
     byte mac[6];
     WiFi.macAddress(mac);
-    sprintf(macAddress, "%02X:%02X:%02X:%02X:%02X:%02X", 
-          mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    sprintf(macAddress, "%02X:%02X:%02X:%02X:%02X:%02X",
+            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
     // Première tentative d'obtention d'ID
     requestID();
   }
 }
 
-bool requestID() {
-  for (int retry = 0; retry < MAX_RETRIES; retry++) {
+bool requestID()
+{
+  for (int retry = 0; retry < MAX_RETRIES; retry++)
+  {
     Serial.print("Tentative ");
     Serial.print(retry + 1);
     Serial.println(" d'obtention d'ID...");
-    
+
     // Utiliser la puce ECCX08 pour générer un token aléatoire sécurisé (4 octets)
     byte randomToken[4];
     ECCX08.random(randomToken, sizeof(randomToken));
-    
+
     // Préparer le message: MAC + token
-    int size_of_MAC = sizeof(macAddress) - 1; // to remove \0 
-    char message[size_of_MAC +1 + sizeof(randomToken)];  // 17 (MAC) + 1 (séparateur) + 4 (token binaire)
+    int size_of_MAC = sizeof(macAddress) - 1;            // to remove \0
+    char message[size_of_MAC + 1 + sizeof(randomToken)]; // 17 (MAC) + 1 (séparateur) + 4 (token binaire)
     memcpy(message, macAddress, size_of_MAC);
-    message[size_of_MAC] = ':';  // Séparateur
-    memcpy(&message[size_of_MAC+1], randomToken, 4);
-    
+    message[size_of_MAC] = ':'; // Séparateur
+    memcpy(&message[size_of_MAC + 1], randomToken, 4);
+
     // Envoyer la demande avec le token
     udp.beginPacket(server_ip, server_port);
-    udp.write((uint8_t*)message, sizeof(message));
+    udp.write((uint8_t *)message, sizeof(message));
     udp.endPacket();
-    
+
     // Attendre la réponse
     unsigned long startTime = millis();
-    while (millis() - startTime < RETRY_TIMEOUT) {
+    while (millis() - startTime < RETRY_TIMEOUT)
+    {
       int packetSize = udp.parsePacket();
       IPAddress remoteIP = udp.remoteIP();
       char remoteIPStr[16]; // Assez grand pour une adresse IP au format xxx.xxx.xxx.xxx
       sprintf(remoteIPStr, "%d.%d.%d.%d", remoteIP[0], remoteIP[1], remoteIP[2], remoteIP[3]);
 
-      if (packetSize > 0 && strcmp(remoteIPStr, server_ip) == 0) {
+      if (packetSize > 0 && strcmp(remoteIPStr, server_ip) == 0)
+      {
         // Format attendu: [ID:1byte][TOKEN:4bytes][MQTT_IP:variable]
-        if (packetSize >= 5) { // Au minimum ID + TOKEN
+        if (packetSize >= 5)
+        { // Au minimum ID + TOKEN
           byte receivedID = udp.read();
-          
+
           // Lire le token retourné (4 bytes)
           byte receivedToken[4];
           udp.read(receivedToken, 4);
-          
+
           // Vérifier que le token correspond
-          if (memcmp(receivedToken, randomToken, 4) == 0) {
+          if (memcmp(receivedToken, randomToken, 4) == 0)
+          {
             deviceID = receivedID;
-            
+
             // Lire l'adresse IP du broker MQTT (reste du paquet)
             int ipLength = packetSize - 5; // Soustraire ID (1) et token (4)
-            if (ipLength > 0 && ipLength < 16) { // Vérification de sécurité
+            if (ipLength > 0 && ipLength < 16)
+            {                          // Vérification de sécurité
               char ipBuffer[16] = {0}; // Assez grand pour "255.255.255.255\0"
-              udp.read((uint8_t*)ipBuffer, ipLength);
+              udp.read((uint8_t *)ipBuffer, ipLength);
               ipBuffer[ipLength] = '\0'; // S'assurer que c'est terminé par un null
-              
+
               // Stocker l'IP du broker
               strcpy(mqttBrokerIP, ipBuffer);
-              
+
               Serial.print("ID authentifié reçu: ");
               Serial.println(deviceID);
               Serial.print("Broker MQTT: ");
               Serial.println(mqttBrokerIP);
               return true;
             }
-          } else {
+          }
+          else
+          {
             Serial.println("Token incorrect, réponse ignorée");
           }
         }
@@ -153,7 +162,7 @@ bool requestID() {
     }
     Serial.println("Timeout, nouvelle tentative...");
   }
-  
+
   Serial.println("Échec de toutes les tentatives. Reprogrammation d'un essai ultérieur.");
   return false;
 }
